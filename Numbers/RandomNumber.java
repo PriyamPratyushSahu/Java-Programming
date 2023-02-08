5f3f9520f142ec9fad97b2e3ea435fc35255ab24
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-08 13:57:58 +0530

    Modified file

--- Numbers/RandomNumber.java
+++ Numbers/RandomNumber.java
@@ -4,7 +4,13 @@ public class RandomNumber {
 
     public static void main(String args[]){
 
-        int n = 10;
-        System.out.println("Random number between 1 to "+ n + ": " + Math.round(Math.random()*n));
+        int n = 100;
+        System.out.println("Random number between 1 to " + n + ": " + Math.round(Math.random() * n));
+        while(true)
+        {
+            int k = (int) Math.round(Math.random()*n);
+            System.out.println("Random number between 1 to "+ n + ": " + k);
+            if(k == 11) break;
+        }
     }
 }

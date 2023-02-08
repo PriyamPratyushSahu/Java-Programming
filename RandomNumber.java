af34985e9c251b7456cd3678ddeeefe3c65ce68b
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-08 12:14:17 +0530

    Added file

---
+++ RandomNumber.java
@@ -0,0 +1,10 @@
+package Numbers;
+
+public class RandomNumber {
+
+    public static void main(String args[]){
+
+        int n = 10;
+        System.out.println("Random number between 1 to "+ n + ": " + Math.round(Math.random()*n));
+    }
+}

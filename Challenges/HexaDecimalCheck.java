a3099f4d709644fd261fdbd79aa62a1e070a074f
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-09 15:09:18 +0530

    Challenge 4

--- Challenges/HexaDecimalCheck.java
+++ Challenges/HexaDecimalCheck.java
@@ -1,5 +1,22 @@
+//Check whether the given number is a binary number or not using regular expression
+
 package Challenges;
 
+import java.util.Scanner;
+
 public class HexaDecimalCheck {
+
+    public static void main(String[] args) 
+    {
+        Scanner pp = new Scanner(System.in);
+        int a;
+
+        System.out.println("Enter a hexadecimal input: ");
+        a = pp.nextInt();
+        
+        String str=String.valueOf(a);
+        System.out.println(str.matches("[0-9A-F]+"));
+        pp.close();
+    }
     
 }

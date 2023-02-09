62a1f3ad239ccab1ca6175d24a208bf3d133606c
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-09 15:37:05 +0530

    Challenge 10

--- Challenges/StringChallenge3.java
+++ Challenges/StringChallenge3.java
@@ -5,5 +5,23 @@ package Challenges;
 import java.util.Scanner;
 
 public class StringChallenge3 {
-    
+
+    public static void main(String[] args) {
+        Scanner pp = new Scanner(System.in);
+
+        String str;
+        System.out.println("Enter a string: ");
+        str = pp.nextLine();
+        str = str.replaceAll("\\s+", " ").trim();
+        String words[]=str.split("\\s");
+        System.out.println("Total number of words: " + words.length);
+        pp.close();
+    }
 }
+
+/*
+ ****************************** OUTPUT ******************************
+Enter a string: 
+The quick brown fox jumps over the lazy dog
+Total number of words: 9
+ */

b432259a45d2d75a070aaefb3a19f6875f917189
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-09 15:19:23 +0530

    Challenge 7

--- Challenges/DateFormatCheck.java
+++ Challenges/DateFormatCheck.java
@@ -1,5 +1,25 @@
 package Challenges;
 
+import java.util.Scanner;
+
 public class DateFormatCheck {
-    
+
+    public static void main(String args[]) {
+        Scanner pp = new Scanner(System.in);
+        String d;
+
+        System.out.println("Enter date in dd/mm/yyyy format: ");
+        d = pp.nextLine();
+
+        String str = String.valueOf(d);
+        System.out.println(str.matches("[0-3][0-9]/[01][0-9]/[0-9]{4}"));
+        pp.close();
+    }
 }
+
+/*
+ **************************** OUTPUT ****************************
+Enter date in dd/mm/yyyy format:
+11/04/2000
+true
+ */

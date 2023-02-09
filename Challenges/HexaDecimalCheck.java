d32b27075ebb27b05facacbd05a5457ed3b8ec26
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-09 15:11:38 +0530

    Challenge 6

--- Challenges/HexaDecimalCheck.java
+++ Challenges/HexaDecimalCheck.java
@@ -6,17 +6,29 @@ import java.util.Scanner;
 
 public class HexaDecimalCheck {
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Scanner pp = new Scanner(System.in);
-        int a;
+        String a;
 
         System.out.println("Enter a hexadecimal input: ");
-        a = pp.nextInt();
-        
-        String str=String.valueOf(a);
+        a = pp.nextLine();
+
+        String str = String.valueOf(a);
         System.out.println(str.matches("[0-9A-F]+"));
         pp.close();
     }
-    
+
 }
+
+/*
+ **************************** OUTPUT ****************************
+ //1
+Enter a hexadecimal input: 
+ABC65EF
+true
+
+//2
+Enter a hexadecimal input: 
+XYS64521
+false
+ */

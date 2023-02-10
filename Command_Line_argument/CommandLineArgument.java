0901f95ac57ffbda48c79adb87d2aa1aa3f1f7a8
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-10 11:11:22 +0530

    Command Line Argument-1

---
+++ Command_Line_argument/CommandLineArgument.java
@@ -0,0 +1,14 @@
+class CommandLineArgument{
+	public static void main(String args[]){
+//Type 1: For no argument in the cmd
+//		System.out.println("Hello World");
+
+//Type 2: Passing argument in the cmd(Here 2 strings are passed)
+//		System.out.println(args[0]);
+//		System.out.println(args[1]);
+
+//Type 3: Length of args is not known
+		for(String x : args) System.out.print(x + " ");
+
+	}
+}

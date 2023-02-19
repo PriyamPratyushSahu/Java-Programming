1260a75d7b290f54bd9016f2b3414b74c2ca78eb
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-19 23:00:34 +0530

    _22_NumberPattern_1

--- Online_Tutorial/Challenges/_22_NumberPattern_1.java
+++ Online_Tutorial/Challenges/_22_NumberPattern_1.java
@@ -15,10 +15,8 @@ public class _22_NumberPattern_1{
 
         int count=0;
         
-      for(int i=1;i<=5;i++)
-      {
-          for(int j=1;j<=5;j++)
-          {
+      for(int i=1;i<=5;i++){
+          for (int j = 1; j <= 5; j++) {
               count++;
             System.out.format("%02d ",count);
           }
--- Online_Tutorial/Challenges/_23_NumberPattern_2.java
+++ Online_Tutorial/Challenges/_23_NumberPattern_2.java
@@ -4,8 +4,22 @@ public class _23_NumberPattern_2{
     public static void main(String[] args) {
         for(int i=1;i<=5;i++)
         {
-            for(int j=1;j<=5;j++){
-                System.out.print(i+j+" ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println("");
+        }
+
+        // Count the total time of loop
+        System.out.println("\nCount the total time of loop: \n");
+        
+        int count=0;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                count++;
+                System.out.print(count+" ");
             }
             System.out.println("");
         }

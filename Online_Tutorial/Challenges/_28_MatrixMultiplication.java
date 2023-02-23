b2e813aac287a3e3f8c1fbb65bc299c83507a793
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-23 15:13:47 +0530

    MatrixMultiplication

--- Online_Tutorial/Challenges/_28_MatrixMultiplication.java
+++ Online_Tutorial/Challenges/_28_MatrixMultiplication.java
@@ -2,13 +2,22 @@ package Challenges;
 
 public class _28_MatrixMultiplication {
 
-    int[][] inputMatix(int arr[][],int r, int c){
-        System.out.println("Two 2d");
+    static int[][]  inputMatix(int arr[][],int r, int c){
+        for(int i = 0; i < arr.length; i++) //row size -> arr2.length     
+            for(int j = 0; j < arr[0].length; j++) //column size -> arr2[0].length
+               arr[i][j] = 1 + (int) Math.round(Math.random() * 5);
         return arr;
     }
 
-    void displayMatrix(int arr[][]){
-        for(int i = 0; i < arr)
+    static void displayMatrix(String s,int arr[][]){
+        System.out.println("\n"+s);
+        System.out.format("Array Size: [%d][%d]\n", arr.length,arr[0].length);
+        for(int i = 0; i < arr.length; i++){
+            for(int j  = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] +" ");
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args) 
     {
@@ -18,41 +27,57 @@ public class _28_MatrixMultiplication {
         int r1,c1,r2,c2;
         int i,j;
 
-        r1 = (int) Math.round(Math.random() * 10);
-        c1 = r2 =  (int) Math.round(Math.random() * 10); //To satisfy matrix multiplication condition
-        c2 = (int) Math.round(Math.random() * 10);
+        r1 = 1 + (int) Math.round(Math.random() * 5);
+        c1 = r2 = 1 + (int) Math.round(Math.random() * 5); //To satisfy matrix multiplication condition
+        c2 = 1 + (int) Math.round(Math.random() * 5);
 
         arr1 = new int [r1][c1];
         arr2 = new int [r2][c2];
+        
         //First matrix
-        arr1
+        arr1 = inputMatix(arr1,r1,c1);
+        displayMatrix("First Matrix",arr1);
 
-        int A[][]={{3,5,9},{7,6,2},{4,3,5}};
-        int B[][]={{1,0,0},{0,1,0},{0,0,1}};
+        //Second matrix
+        arr2 = inputMatix(arr2,r2,c2);
+        displayMatrix("Second Matrix",arr2);
         
-        int C[][]=new int[3][3];
+        int arr3[][]=new int[r1][c2];
     
-        for(int i=0;i<3;i++)
-        {
-            for(int j=0;j<3;j++)
-            {
-                C[i][j]=0;
-                for(int k=0;k<3;k++)
-                {
-                    C[i][j]=C[i][j]+A[i][k]*B[k][j];
-                }
+        for(i = 0 ; i < r1 ;i++){
+            for(j = 0 ; j < c2 ; j++){
+                arr3[i][j]=0;
+                for(int k=0 ; k < r2; k++) //or k < c1
+                    arr3[i][j] +=  arr1[i][k] * arr2[k][j];
             }
         }
-        
-        for(int x[]:C)
-        {
-            for(int y:x)
-            {
-                System.out.print(y+" ");
-            }
-            System.out.println("");
-        }
-    
+        displayMatrix("Result Matrix",arr3);
     }
-    
 }
+/*
+ ************************** OUTPUT **************************
+ First Matrix
+Array Size: [5][6]
+4 4 3 3 6 3
+1 3 5 6 4 3
+4 5 5 5 4 4
+4 2 5 3 3 5
+4 3 5 1 5 4
+
+Second Matrix
+Array Size: [6][3]
+2 3 4
+4 5 3
+5 6 4
+2 3 2
+6 4 4
+6 3 1
+
+Result Matrix
+Array Size: [5][3]
+99 92 73
+93 91 64
+111 110 81
+95 88 65
+101 92 71
+ */

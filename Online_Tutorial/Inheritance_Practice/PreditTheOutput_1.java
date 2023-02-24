334a707319ca0885cd8184aa67fdc874dce8a784
Priyam <priyampratyushsahu3232@gmail.com>
2023-02-24 13:48:04 +0530

    Dynamic Method Dispatch

---
+++ Online_Tutorial/Inheritance_Practice/PreditTheOutput_1.java
@@ -0,0 +1,19 @@
+package Inheritance_Practice;
+
+class X{
+    int a = 10;
+}
+
+class Y extends X{
+    int a = 20;
+}
+
+
+public class PreditTheOutput_1 {
+    public static void main(String args[]) {
+
+        X x = new Y(); //Dynamic Method Dispatch
+        System.out.println(x.a); //10
+    }
+}
+

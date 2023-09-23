cf7d56bb2c22a7a51d7defaff97188edcdde34bc
unknown <priyampratyushsahu3232@gmail.com>
2023-09-23 14:53:34 +0530

    'Update'

--- Complete_Chapterwise_Concept_And_Code/_00_Miscellaneous/_01_Bit_Manipulation/_08_Prog.java
+++ Complete_Chapterwise_Concept_And_Code/_00_Miscellaneous/_01_Bit_Manipulation/_08_Prog.java
@@ -25,7 +25,12 @@ public class _08_Prog {
 
 /*
  Explanation
- 17 -> 10001 ( 2 bits are having 1)
- 6 -> 110 ( 2 bits are having 1)
- 49 -> 11001 ( 3 bits are having 1)
+ -> num > 0: This part checks whether num is greater than 0.
+    This is important because powers of two are always positive integers.
+    If num is not greater than 0 (i.e., it's zero or negative), the function immediately returns false.
+
+ ->(num & (num - 1)) == 0: This is the core of the logic to check if num is a power of two.
+   (num - 1) subtracts 1 from num. When you subtract 1 from a binary number, 
+   it flips the least significant 1 bit to 0 and all the trailing 0s to 1s.
+   For example, if num is 8 (binary: 1000), (num - 1) is 7 (binary: 0111).
  */

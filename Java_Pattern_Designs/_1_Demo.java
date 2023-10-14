ca5510fcdf4d5f8d1319a6b1f4104ef297e3d06f
unknown <priyampratyushsahu3232@gmail.com>
2023-10-14 20:27:34 +0530

    Update

--- Java_Pattern_Designs/_1_Demo.java
+++ Java_Pattern_Designs/_1_Demo.java
@@ -6,8 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
